f302c8346ecca1a6c0c32d62201ff9ac51385752
“maitreverge_42” <verge.f@gmail.com>
2024-05-19 15:38:17 +0200

    add libft on master

---
+++ ft_itoa.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 11:58:05 by prossi            #+#    #+#             */
+/*   Updated: 2021/09/22 19:44:23 by prossi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned int	ft_number_size(int number)
+{
+	unsigned int	length;
+	
+	length = 0;
+	if (number == 0)
+		return (1);
+	if (number < 0)
+		length += 1;
+	while (number != 0)
+	{
+		number /= 10;
+		length++;
+	}
+	return (length);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*string;
+	unsigned int	number;
+	unsigned int	length;
+
+	length = ft_number_size(n);
+	string = (char *)malloc(sizeof(char) * (length + 1));
+	if (string == NULL)
+		return (NULL);
+	if (number < 0)
+	{
+		string[0] = '-';
+		number = -n;
+	}
+	else
+		number = n;
+	if (number == 0)
+		string[0] = '0';
+	string[length] = '\0';
+	while (number != 0)
+	{
+		string[length - 1] = (number % 10) + '0';
+		number = number / 10;
+		length--;
+	}
+	return (string);
+}
